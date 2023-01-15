36824a59d2cbcb7a1560069792b3df87098915e3
iwanromanoff <iwnroff@mail.ru>
2023-01-15 20:35:57 +0300

    fix

--- src/iwnroff/status_monitor_service.clj
+++ src/iwnroff/status_monitor_service.clj
@@ -1,12 +1,44 @@
 (ns iwnroff.status-monitor-service
-  (:gen-class))
+  (:gen-class)
+  (:require [org.httpkit.server :as app-server]
+            [compojure.core :refer [defroutes GET]]))
 
-(defn greet
-  "Callable entry point to the application."
-  [data]
-  (println (str "Hello, " (or (:name data) "World") "!")))
+(defonce app-server-instance (atom nil))
+
+(defroutes status-monitor
+  (GET "/" [] {:status 200 :body "Status Monitor Dashboard"}))
+
+(defn stop-app-server
+  "Gracefully shutdown the server, waiting 100ms"
+  []
+  (when-not (nil? @app-server-instance)
+    (@app-server-instance :timeout 100)
+    (reset! app-server-instance nil)
+    (println "INFO: Application server stopped")))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (greet {:name (first args)}))
+  "Start the application server and run the application"
+  [port]
+  (println "INFO: Starting server on port: " port)
+  (reset! app-server-instance
+          (app-server/run-server #'status-monitor {:port (Integer/parseInt port)})))
+
+
+(defn restart-app-server
+  "Convenience function to stop and start the application server"
+  []
+  (stop-app-server)
+  (-main "8070")) 
+
+(comment
+
+  ;; start application
+  (-main "8070")
+  
+  (+ 1 2)
+
+  ;; stop application
+  (stop-app-server)
+
+  ;; restart application
+  (restart-app-server))
